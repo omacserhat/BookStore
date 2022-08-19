1ae7b17a6fc2c37f0afc865a33e278e1ff05c22f
Serhat OMAÇ <omacserhatt@gmail.com>
2022-08-19 18:12:09 +0300

    Delete endpoint'inin yazılması

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -86,5 +86,18 @@ namespace BookStore.Controllers
 
             return Ok();
         }
+
+        //delete
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+                return BadRequest();
+
+            BookList.Remove(book);
+            return Ok();
+        }
     }
 }
