8585cde543bef20a65e59d2f47f0435404834ac5
Serhat OMAÇ <omacserhatt@gmail.com>
2022-08-19 16:38:26 +0300

    Get ve GetById endpoint'lerinin yazılması

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -1,9 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BookStore.Controllers
 {
+    [ApiController]
+    [Route("[controller]s")]
     public class BookController : ControllerBase
     {
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1, // Personal Growth
+                PageCount = 200,
+                PublishDate = new System.DateTime(2001,06,12)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Herland",
+                GenreId = 2, // Science Fiction
+                PageCount = 250,
+                PublishDate = new System.DateTime(2010,05,23)
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Dune",
+                GenreId = 2, // Science Fiction
+                PageCount = 540,
+                PublishDate = new System.DateTime(2001,12,21)
+            }
+        };
 
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+        /*
+        [HttpGet]
+        public Book Get([FromQuery] string id)
+        {
+            var bookList = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
+            return bookList;
+        }
+        */
     }
 }
