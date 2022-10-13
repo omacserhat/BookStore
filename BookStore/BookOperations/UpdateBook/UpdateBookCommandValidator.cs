5d36782a9476baba0ee3b4a2982e61335039cdd3
Serhat OMAÇ <omacserhatt@gmail.com>
2022-10-13 13:44:44 +0300

    Eksik validasyon eklendi

--- BookStore/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
+++ BookStore/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
@@ -6,8 +6,10 @@ namespace BookStore.BookOperations.UpdateBook
     {
         public UpdateBookCommandValidator()
         {
+            RuleFor(command => command.BookId).GreaterThan(0);
             RuleFor(command => command.Model.GenreId).GreaterThan(0);
             RuleFor(command => command.Model.Title).NotEmpty().MinimumLength(4);
+
         }
     }
 }
